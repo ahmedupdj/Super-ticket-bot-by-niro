5bb0bf3b5a493d3368848f001dbfcc9010680b2d
NIR0 <74272765+NIR0-V@users.noreply.github.com>
2020-12-31 21:50:57 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,6 @@ module.exports.run = async (bot, message, args) => {
     .addField("Adding a User To The Ticket [Alias: a]", "`ticket.add [usermention]`")
     .addField("Removing a User From The Ticket [Alias: r]", "`ticket.remove [usermention]`")
     .addField("Last Ticket Information [Alias: lt]", "`ticket.last`")
-    .addField("ADMIN | Force Closing The Ticket [Alias: fc]", "`ticket.forceclose [reason]`")
     .addField("ADMIN | Rename a Ticket [Alias: rn]", "`ticket.rename [name]`")
     .addField("ADMIN | Timing Out a Ticket [Alias: to]", "`ticket.timeout`")
     .addField("ADMIN | Setting Up Ticket Topics [Alias: s]", "`ticket.set [1 - 5] [Topic]`")
